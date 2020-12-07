dc463e9f40e79d1f4752eb6bc97624efe6ebd867
Noah Roberts <noahmroberts@gmail.com>
2020-12-07 10:45:48 -0600

    store added
    Added questions to store array, changed title and header

--- app.js
+++ app.js
@@ -4,25 +4,55 @@
 const store = {
   // 5 or more questions are required
   questions: [
-    {
-      question: 'What color is broccoli?',
+    { // question 1
+      question: 'Commonly used as a malaria medication, what is the name of the compound that gives gin & tonics their characteristic bitterness?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'caffeine', 
+        'wormwood',
+        'quinine',
+        'gentian'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'quinine'
     },
-    {
-      question: 'What is the current year?',
+    { // question 2
+      question: 'Which spirit is traditionally used as the base of a gimlet?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'rum',
+        'gin',
+        'bourbon',
+        'tequila'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'gin'
+    },
+    { // question 3
+      question: 'Along with rum, sugar, lime, and soda water, what is the final ingredient used to build and garnish a mojito?',
+      answers: [
+        'bitters',
+        'grenadine',
+        'orange twist',
+        'mint'
+      ],
+      correctAnswer: 'mint'
+    },
+    { // question 4
+      question: 'What ingredient is added to make a "perfect" manhattan?',
+      answers: [
+        'dry vermouth',
+        'absinthe',
+        'orange bitters',
+        'green Chartreuse'
+      ],
+      correctAnswer: 'dry vermouth'
+    },
+    { // question 5
+      question: 'What does it mean to serve a cocktail "up"?',
+      answers: [
+        'on ice',
+        'chilled, then strained and served in a stemmed glass',
+        'at room temperature',
+        'with a topper of champagne'
+      ],
+      correctAnswer: 'chilled, then strained and served in a stemmed glass'
     }
   ],
   quizStarted: false,
